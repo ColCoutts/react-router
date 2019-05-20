c5b66a23f1c28529d6551891b5568c4cdbf8fc9f
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-20 11:31:30 -0700

    built components now for test

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-i
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Header from './Header';
+import Red from './Red';
+import Blue from './Blue';
+import Yellow from './Yellow';
+
+export default function App() {
+  return (
+    <Router>
+      <> 
+      <Header />
+      <Switch>
+        <Route path="/red" component={Red}/>
+        <Route path="/blue" component={Blue}/>
+        <Route path="/yellow" component={Yellow}/>
+      </Switch>
+      </>
+    </Router>
+  );
+}
---
+++ src/components/Red.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Red() {
+  const style = {
+    backgroundColor: 'red',
+    height: '100vh',
+    width: '100vw'
+  };
+
+  return <div style={style}></div>;
+}
